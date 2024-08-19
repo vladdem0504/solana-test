12003086d57c918061e9953a21b2463a0c497128
vladdem0504 <vladimir@wyndlabs.ai>
2024-08-20 00:24:29 +0500

    compati with anchor 20

--- program-test/src/lib.rs
+++ program-test/src/lib.rs
@@ -183,6 +183,95 @@ pub fn invoke_builtin_function(
     Ok(0)
 }
 
+// Alternative to ProcessInstruction, but with lifetimes compatible with anchor 0.30 entrypoints
+// Workaround for https://github.com/coral-xyz/anchor/pull/2711
+pub type AnchorProcessInstruction = for<'info> fn(
+    program_id: &Pubkey,
+    accounts: &'info [AccountInfo<'info>],
+    instruction_data: &[u8],
+) -> ProgramResult;
+
+pub fn invoke_builtin_anchor_function(
+    builtin_function: AnchorProcessInstruction,
+    invoke_context: &mut InvokeContext,
+) -> Result<u64, Box<dyn std::error::Error>> {
+    set_invoke_context(invoke_context);
+
+    let transaction_context = &invoke_context.transaction_context;
+    let instruction_context = transaction_context.get_current_instruction_context()?;
+    let instruction_data = instruction_context.get_instruction_data();
+    let instruction_account_indices = 0..instruction_context.get_number_of_instruction_accounts();
+
+    let log_collector = invoke_context.get_log_collector();
+    let program_id = instruction_context.get_last_program_key(transaction_context)?;
+    stable_log::program_invoke(
+        &log_collector,
+        program_id,
+        invoke_context.get_stack_height(),
+    );
+
+    // Copy indices_in_instruction into a HashSet to ensure there are no duplicates
+    let deduplicated_indices: HashSet<IndexOfAccount> = instruction_account_indices.collect();
+
+    // Serialize entrypoint parameters with SBF ABI
+    let (mut parameter_bytes, _regions, _account_lengths) = serialize_parameters(
+        invoke_context.transaction_context,
+        invoke_context
+            .transaction_context
+            .get_current_instruction_context()?,
+        true, // should_cap_ix_accounts
+        true, // copy_account_data // There is no VM so direct mapping can not be implemented here
+    )?;
+
+    // Deserialize data back into instruction params
+    let (program_id, account_infos, _input) =
+        unsafe { deserialize(&mut parameter_bytes.as_slice_mut()[0] as *mut u8) };
+
+    // Execute the program
+    builtin_function(program_id, &account_infos, instruction_data).map_err(|err| {
+        let err = InstructionError::from(u64::from(err));
+        stable_log::program_failure(&log_collector, program_id, &err);
+        let err: Box<dyn std::error::Error> = Box::new(err);
+        err
+    })?;
+    stable_log::program_success(&log_collector, program_id);
+
+    // Lookup table for AccountInfo
+    let account_info_map: HashMap<_, _> =
+        account_infos.iter().map(|a| (*a.key, a.clone())).collect();
+
+    // Re-fetch the instruction context. The previous reference may have been
+    // invalidated due to the `set_invoke_context` in a CPI.
+    let transaction_context = &invoke_context.transaction_context;
+    let instruction_context = transaction_context.get_current_instruction_context()?;
+
+    // Commit AccountInfo changes back into KeyedAccounts
+    for i in deduplicated_indices.into_iter() {
+        let mut borrowed_account =
+            instruction_context.try_borrow_instruction_account(transaction_context, i)?;
+        if borrowed_account.is_writable() {
+            if let Some(account_info) = account_info_map.get(borrowed_account.get_key()) {
+                if borrowed_account.get_lamports() != account_info.lamports() {
+                    borrowed_account.set_lamports(account_info.lamports())?;
+                }
+
+                if borrowed_account
+                    .can_data_be_resized(account_info.data_len())
+                    .is_ok()
+                    && borrowed_account.can_data_be_changed().is_ok()
+                {
+                    borrowed_account.set_data_from_slice(&account_info.data.borrow())?;
+                }
+                if borrowed_account.get_owner() != account_info.owner {
+                    borrowed_account.set_owner(account_info.owner.as_ref())?;
+                }
+            }
+        }
+    }
+
+    Ok(0)
+}
+
 /// Converts a `solana-program`-style entrypoint into the runtime's entrypoint style, for
 /// use with `ProgramTest::add_program`
 #[macro_export]
@@ -197,6 +286,26 @@ macro_rules! processor {
                 $crate::invoke_builtin_function($builtin_function, vm.context_object_pointer)
                     .map_err(|err| $crate::EbpfError::SyscallError(err))
                     .into();
+        })
+    };
+}
+
+/// Converts a `anchor-program`-style entrypoint into the runtime's entrypoint style, for
+/// use with `ProgramTest::add_program`
+#[macro_export]
+macro_rules! anchor_processor {
+    ($builtin_function:expr) => {
+        Some(|vm, _arg0, _arg1, _arg2, _arg3, _arg4| {
+            let vm = unsafe {
+                &mut *((vm as *mut u64).offset(-($crate::get_runtime_environment_key() as isize))
+                    as *mut $crate::EbpfVm<$crate::InvokeContext>)
+            };
+            vm.program_result = $crate::invoke_builtin_anchor_function(
+                $builtin_function,
+                vm.context_object_pointer,
+            )
+            .map_err(|err| $crate::EbpfError::SyscallError(err))
+            .into();
         })
     };
 }
